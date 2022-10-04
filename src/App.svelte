b28763e0c61220c5147d6e431b464e4dd5ab8d97
Michal Kurzewski <64260000+MichalKurzewski@users.noreply.github.com>
2022-10-04 08:28:24 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<main>
+ <div>
+  <h1> hello world</h1>
+ <Footer/>
+ </div>
+</main>
+
+<style>
+ 
+</style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+    <div class="copyright">Copyright 2022</div>
+</footer>
+<style> 
+footer{
+    padding: 40px;
+    text-align: center;
+}
+.copyright{
+    font-size: 12px;
+    color: #ccc;
+    border-top: 1px solid #ddd;
+    padding: 20px;
+    display: inline-block;
+}
+</style>
