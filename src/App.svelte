1ce460bdb07210399c569795c9c73bfe4d708dd4
Michal Kurzewski <64260000+MichalKurzewski@users.noreply.github.com>
2022-10-04 18:07:15 +0100

    basics

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   import Footer from "./components/Footer.svelte";
+  import Input from "./components/Input.svelte";
 </script>
 
 <main>
  <div>
   <h1>hello world</h1>
+  <Input/>
  <Footer/>
  </div>
 </main>
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,9 +1,18 @@
 <script>
+let firstNumber = getRandomIntInclusive(50,1000);
+let secondNumber = getRandomIntInclusive(10,100);
+let result;
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 </script>
-<div>
-    
-</div>
+<span>
+    <h1>{firstNumber} + {secondNumber} = </h1>
+    <input value={result}/>
+</span>
 <style>
 
 </style>
